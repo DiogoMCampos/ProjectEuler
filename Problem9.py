418593859543880b5e01f853981453f1899e523e
DiogoMCampos <d.diogocampos@gmail.com>
2016-02-02 09:29:05 +0000

    Fixed a typo in Problem 9

--- Problem9.py
+++ Problem9.py
@@ -13,7 +13,7 @@ from math import sqrt
 def problem9():
     found = False
     for a in range(1, 999):
-        # since a != 0, b != 0, c 1=, 998 is the maximum for each
+        # since a != 0, b != 0, c != 0, 998 is the maximum for each
         # since (500 - a - b) must be negative, a + b > 500
         # therefore, for each a, b > 500 - a
         for b in range(500 - a, 999):
