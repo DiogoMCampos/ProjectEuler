784a62b9f0a3998b82606078bcf8b53db8c818f7
DiogoMCampos <d.diogocampos@gmail.com>
2016-02-03 20:15:08 +0000

    Problem 10 Solved using Sieve of Eratosthenes

---
+++ Problem10.py
@@ -0,0 +1,23 @@
+from math import sqrt
+
+def problem10():
+    # based on the Sieve of Eratosthenes
+    numberList = [True] * 2000000
+    primesSum = 0
+
+    numberList[0] = numberList[1] = False
+
+    for (i, prime) in enumerate(numberList):
+        if prime:
+            primesSum += i
+            
+            if i <= sqrt(2000000):
+                j = i * i
+                while j < 2000000:
+                    numberList[j] = False
+                    j += i
+
+    print(primesSum)
+
+
+problem10()
